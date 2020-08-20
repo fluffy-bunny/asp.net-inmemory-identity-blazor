70606613a8c393c6dad370ff2eceff9555ecf59e
Herb Stahl <ghstahl@gmail.com>
2020-08-20 12:04:51 -0700

    missing files

---
+++ src/InMemoryIdentityApp/Pages/BlazorHost.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace InMemoryIdentityApp.Pages
+{
+    public class BlazorHostModel : PageModel
+    {
+        private readonly ILogger<BlazorHostModel> _logger;
+
+        public BlazorHostModel(ILogger<BlazorHostModel> logger)
+        {
+            _logger = logger;
+        }
+        public string BlazorId;
+       
+        public void OnGet(string id)
+        {
+            BlazorId = id;
+        }
+    }
+}
