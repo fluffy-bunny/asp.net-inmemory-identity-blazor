2adf57d215087ca51b97bb17c6adef97a30c51d7
Herb Stahl <ghstahl@gmail.com>
2020-08-25 15:39:43 -0700

    missing

---
+++ src/OAuth2.TokenManagement.Client/TokenManagerAuthHandlerStateSink.cs
@@ -0,0 +1,24 @@
+﻿using ClientSideAuth.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAuth2.TokenManagement.Client
+{
+    class TokenManagerAuthHandlerStateSink : IAuthHandlerStateSink
+    {
+        private ITokenManager _tokenManager;
+
+        public TokenManagerAuthHandlerStateSink(ITokenManager tokenManager)
+        {
+            _tokenManager = tokenManager;
+        }
+        public async Task OnAuthenticatedAsync(bool authenticated)
+        {
+            if (!authenticated) { }
+            await _tokenManager.RemoveAllConcurrentManagedTokenAsync(); 
+        }
+ 
+    }
+}
