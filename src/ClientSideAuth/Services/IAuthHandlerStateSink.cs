3abffda451422527e3f2f4676c078759806e85d7
Herb Stahl <ghstahl@gmail.com>
2020-08-25 15:35:54 -0700

    missing

---
+++ src/ClientSideAuth/Services/IAuthHandlerStateSink.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace ClientSideAuth.Services
+{
+    public interface IAuthHandlerStateSink
+    {
+        Task OnAuthenticatedAsync(bool authenticated);
+    }
+}
